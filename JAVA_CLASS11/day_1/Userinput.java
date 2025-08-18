536c2212c4a747092f4b4577b285671eccf1a24c
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-08-18 13:41:03 +0545

    added 2 files

---
+++ JAVA_CLASS11/day_1/Userinput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInputExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your marks: ");
+        double marks = scanner.nextDouble();
+
+        System.out.println("Hello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
+        System.out.println("Your marks: " + marks);
+
+        scanner.close();
+    }
+}
+
